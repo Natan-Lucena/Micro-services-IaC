cbcf20b259a7ea4ad2b83eabdeaa34872aed1f20
Natan Lucena <zerogamerptbr@gmail.com>
2025-05-18 15:08:02 -0300

    :sparkles: feat: update VPC name to maintain consistency in naming conventions

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source               = "terraform-aws-modules/vpc/aws"
   version              = "2.77.0"
-  name                 = "rabbit_mq_vpc"
+  name                 = "my-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   public_subnets       = ["10.0.1.0/24", "10.0.2.0/24"]
